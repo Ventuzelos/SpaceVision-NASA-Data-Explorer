6db9255d407fe4ec0f77019d366a92e9a7fa26d2
Cristina <angela.pereira.263800148@msft.cesae.pt>
2026-07-08 12:44:27 +0100

    style: inicia centralização dos ícones com Lucide

--- frontend/src/components/home/Hero/Hero.jsx
+++ frontend/src/components/home/Hero/Hero.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Icons } from "../../../constants/icons";
+
 import Button from "../../common/Button/Button";
-import heroImage from "../../../assets/hero2.jpg";
 import SearchInput from "../../common/SearchInput/SearchInput";
+import heroImage from "../../../assets/hero2.jpg";
 import { searchablePages } from "../../../constants/searchPages";
+
 import "./Hero.css";
 
+
+const searchIcons = {
+  image: Icons.Image,
+  sun: Icons.Sun,
+  earth: Icons.Globe,
+  meteor: Icons.Satellite,
+};
 
 function Hero() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -48,19 +58,29 @@ function Hero() {
 
               {filteredPages.length > 0 && (
                 <div className="hero__search-results">
-                  {filteredPages.map((page) => (
-                    <Link
-                      key={page.path}
-                      to={page.path}
-                      className="hero__search-result"
-                    >
-                      <div className="hero__search-result-header">
-                        <span className="hero__search-result-icon">{page.icon}</span>
-                        <strong>{page.title}</strong>
-                      </div>
-                      <span>{page.subtitle}</span>
-                    </Link>
-                  ))}
+                  {filteredPages.map((page) => {
+                    const Icon = searchIcons[page.icon];
+
+                    return (
+                      <Link
+                        key={page.path}
+                        to={page.path}
+                        className="hero__search-result"
+                      >
+                        <div className="hero__search-result-header">
+                          {Icon && (
+                            <Icon
+                              className="hero__search-result-icon"
+                              size={18}
+                            />
+                          )}
+                          <strong>{page.title}</strong>
+                        </div>
+
+                        <span>{page.subtitle}</span>
+                      </Link>
+                    );
+                  })}
                 </div>
               )}
             </div>
