251dd2831b419d99725af7aba97273b9b54046b3
Isabel <isabel.dias.250929244@msft.cesae.pt>
2026-07-10 09:32:39 +0100

    feat: adiciona rotas, controllers e proteção da API key

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,7 @@
-// const NASA_API_KEY = "TxaaZFCkFzyFosJIPfKWYFRuJhtTBVMBxhSxgOl2";
-
-// const BASE_URL = "https://api.nasa.gov";
-
-// export { NASA_API_KEY, BASE_URL };
-
 import axios from "axios";
 
-const NASA_API_KEY = import.meta.env.VITE_NASA_API_KEY || "TxaaZFCkFzyFosJIPfKWYFRuJhtTBVMBxhSxgOl2";
-const NASA_BASE_URL = "https://api.nasa.gov";
-
 const api = axios.create({
-  baseURL: NASA_BASE_URL,
-  params: {
-    api_key: NASA_API_KEY,
-  },
+  baseURL: "http://127.0.0.1:8000/api/nasa",
 });
 
-export { NASA_API_KEY, NASA_BASE_URL };
 export default api;
--- frontend/src/services/apodService.js
+++ frontend/src/services/apodService.js
@@ -1,12 +1,6 @@
 import api from "./api";
 
 export async function getApod() {
- const { data } = await api.get("/planetary/apod");
-return data;
-
-  if (!response.ok) {
-    throw new Error("Erro ao carregar a imagem do dia.");
-  }
-
-  return response.json();
+  const { data } = await api.get("/apod");
+  return data;
 }
