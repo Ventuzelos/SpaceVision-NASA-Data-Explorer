6ecdbf201053b10c364de75d79cd40bb077b0e51
DianaS <diana.silva.257306188@msft.cesae.pt>
2026-07-14 14:37:13 +0100

    feat: torna a linha do tempo responsiva e vertical no mobile

--- frontend/src/pages/DISCOVR/DISCOVR.jsx
+++ frontend/src/pages/DISCOVR/DISCOVR.jsx
@@ -1,6 +1,5 @@
 import Container from "../../components/common/Container/Container";
 import DiscovrHero from "../../components/DISCOVR/DiscovrHero/DiscovrHero";
-import DiscovrSectionNav from "../../components/DISCOVR/DiscovrSectionNav/DiscovrSectionNav";
 import DiscovrGallery from "../../components/DISCOVR/DiscovrGallery/DiscovrGallery";
 import DiscovrTimeline from "../../components/DISCOVR/DiscovrTimeline/DiscovrTimeline";
 import DiscovrAsteroidRadar from "../../components/DISCOVR/DiscovrAsteroidRadar/DiscovrAsteroidRadar";
@@ -12,7 +11,7 @@ function DISCOVR() {
   return (
     <main className="discovr-page">
       <DiscovrHero />
-      <DiscovrSectionNav />
+      
 
       <Container>
         <DiscovrGallery />
