17e0053dd9667db54d84d9710063ebb69d06ce67
DiGsilva88 <diana_gomesilva@hotmail.com>
2026-07-22 23:47:32 +0100

    novos logos adicionados

--- frontend/src/components/layout/Logo/Logo.jsx
+++ frontend/src/components/layout/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../../assets/logos/logo-horizontal.svg";
+import logo from "../../../assets/logos/logo-horizontal-planet.svg";
 
 import "./Logo.css";
 
