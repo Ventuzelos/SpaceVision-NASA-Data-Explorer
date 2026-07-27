7cb1f5e7697213d88939f09b09673fd52cd3cc08
DiGsilva88 <diana_gomesilva@hotmail.com>
2026-07-27 13:39:02 +0100

    corrigir paddings -user flow-logos

--- frontend/src/components/home/ApiSection/ApiIcons/ApiIcons.jsx
+++ frontend/src/components/home/ApiSection/ApiIcons/ApiIcons.jsx
@@ -5,13 +5,20 @@ import "./ApiIcons.css";
 function ApiIcons({ apis }) {
   return (
     <div className="api-icons">
-      {apis.map(({ title, description, category, icon: Icon, link }) => (
+      {apis.map(({ title, description, category, icon: Icon, link, isLiveApi }) => (
         <Link key={title} to={link} className="api-icons__item">
           <span className="api-icons__icon">
             {Icon && <Icon size={28} aria-hidden="true" />}
           </span>
 
           <span className="api-icons__name">{title}</span>
+
+          {isLiveApi && (
+            <span className="api-icons__live-badge">
+              <span className="api-icons__live-pulse" aria-hidden="true" />
+              Ao vivo
+            </span>
+          )}
 
           {category && (
             <span className="api-icons__category">{category}</span>
